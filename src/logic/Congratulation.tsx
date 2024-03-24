eb624934516e0094caf14bb432c5d9d19d6fa419
born <born@ecwid.com>
2024-03-24 15:24:11 +0400

    Changed icon.

--- src/logic/Congratulation.tsx
+++ src/logic/Congratulation.tsx
@@ -1,25 +1,28 @@
 export const initialText = [
-    { text: 'Мя! Котоматрица активирована!', isCommand: true, typed: true },
+    { text: 'Матрица активирована!', isCommand: true, typed: true },
     { text: 'Войти?', isCommand: true, typed: true },
     { text: 'Да', isCommand: true, typed: true },
     { text: 'Секундочку ......', isCommand: true, typed: true },
     { text: 'Пожалуй начну с идентификации', isCommand: true, typed: true },
-    { text: 'Посмотри-ка в камеру ............', isCommand: true, typed: true },
+    { text: 'Ну-ка посмотри-ка в камеру ...................', isCommand: true, typed: true },
     { text: 'Сканирование завершено!', isCommand: true, typed: true },
 ];
 
 export const inboxTexts = [
     { text: 'Привет, Юлёк!', isCommand: true, typed: true },
-    { text: 'У тебя тут входящее есть от кое-кого, проверишь?', isCommand: true, typed: true },
+    { text: 'У тебя тут входящее есть от кое-кого :)', isCommand: true, typed: true },
+    { text: 'Может проверишь?', isCommand: true, typed: true },
     { text: 'Конечно!', isCommand: true, typed: true },
-    { text: 'Секундочку ........', isCommand: true, typed: true },
+    { text: 'Секундочку. Загружаю ........', isCommand: true, typed: true },
 ]
 
 export const congratulationText = [
     { text: 'Дорогая, Юля!', isCommand: false, typed: false },
-    { text: 'Желаю тебе всего самого в твоем Новом Году!', isCommand: false, typed: false },
-    { text: 'Побольше ярких эмоций и интересных проектов!', isCommand: false, typed: false },
-    { text: 'Кот-Бегемот ♡', isCommand: false, typed: false },
+    { text: 'Желаю Тебе всего самого в Новом Году!', isCommand: false, typed: false },
+    { text: 'Хочу чтобы Ты радовалась жизни!', isCommand: false, typed: false },
+    { text: 'Чтобы у Тебя все получалось!', isCommand: false, typed: false },
+    { text: 'Лёша с ♡♡♡', isCommand: false, typed: false },
+    { text: 'P.S. я тут еще пасхалку оставил одну - найди ее :)', isCommand: false, typed: false },
 ]
 
 export default {};
